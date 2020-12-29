35f1fcbd8f0a5bfcc30701b9123bc976efad1ac6
wenfangping@g7.com.cn <wenfangping@g7.com.cn>
2020-12-29 15:32:16 +0800

    feat: update

--- bin/main.js
+++ bin/main.js
@@ -7,8 +7,13 @@ program.command('init <name>').description('init project').action(name => {
     console.log('init ' + name)
     require('../lib/init')(name);
 })
+
 program.command('refresh').description('refresh').action(() => {
     require('../lib/refresh')()
+})
+
+program.command('create <name>').description('Pull the basic warehouse according to the type.').action(name => {
+    require('../lib/create')(name)
 })
 
 program.parse(process.argv);
